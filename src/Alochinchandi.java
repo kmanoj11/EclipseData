3215372e5ac597418841612279368ff8dc343bfb
kmanoj11 <manimanoj2411@gmail.com>
2024-09-20 10:52:03 +0530

    adding

--- src/Alochinchandi.java
+++ src/Alochinchandi.java
@@ -1,4 +1,5 @@
 
-public class Alochinchandi {
+public class Alochinchandi
+{
 
 }
---
+++ src/Manchodu2.java
@@ -0,0 +1,19 @@
+
+public class Manchodu2 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println(new Manchodu2().arif());
+
+	}
+	String arif()
+	{
+		return "arif";
+	}
+     int a = 10;
+     {
+    	
+    	 
+     }
+}
